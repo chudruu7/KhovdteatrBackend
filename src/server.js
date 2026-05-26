4b2ddb0ae607c7123fb88832395e1b2a9e27fe58
Tumurchudur <163247289+chudruu7@users.noreply.github.com>
2026-05-26 11:08:31 +0700

    last baigasa

--- src/server.js
+++ src/server.js
@@ -79,7 +79,7 @@ app.use(cors({
     }
   },
   credentials: true,  // ← энэ заавал байх ёстой!
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
